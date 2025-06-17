6d8aa08e569272db9b81df400e2dbfa5e6805851
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2025-06-17 09:45:47 -0700

    College WR 2024 data

--- src/data_processing.py
+++ src/data_processing.py
@@ -7,7 +7,7 @@ import re
 import time
 from pathlib import Path
 
-YEARS_TO_SCRAPE = range(2015, 2025)
+YEARS_TO_SCRAPE = range(2024, 2025)
 
 SECONDS_PER_REQUEST = 2
 
@@ -17,15 +17,16 @@ def scrape_all_draft_data(years_to_scrape: range) -> pd.DataFrame:
     #helps with our requests
     preURL = 'https://www.sports-reference.com/cfb/'
     for year in YEARS_TO_SCRAPE:
-        time.sleep(5)
+        time.sleep(3)
         try:
             url = preURL+ "years" + "/" + str(year) + '-receiving.html'
-            time.sleep(5)
+            time.sleep(3)
             response = requests.get(url)
             response.raise_for_status()
             soup = BeautifulSoup(response.content, 'html.parser')
            
             #This gets us the data for all receivers in a year into a BeautifulSoup Object
+
             table = soup.find('table',{'id': 'receiving_standard'})
 
             #Gives the column labels so we know how to seperate our information
@@ -35,13 +36,14 @@ def scrape_all_draft_data(years_to_scrape: range) -> pd.DataFrame:
             table_body= table.find('tbody')
             rows = table_body.find_all('tr')
 
-            for row in rows:
+            for row in rows[:-1]:
                 #This first line may not be needed I doubt it
                 cells = row.find_all('td')
                 firstColumn = row.find('td')
 
                 #Use the table to find the player receiving yards in a certain year
                 player_receiving_yards = int(row.find('td', {'data-stat': 'rec_yds'}).text.strip())
+                print(player_receiving_yards)
                 
                 
                 a_tag = firstColumn.find('a')
@@ -58,9 +60,12 @@ def scrape_all_draft_data(years_to_scrape: range) -> pd.DataFrame:
 
                     #Get player rookie year href?
                     # player_rookie_href = lastName_href_list(player_name)
-                    player_record = get_college_player(player_college_href, player_name, player_receiving_yards)
-                    if player_record['position'] == 'WR':
-                        all_player_records.append(player_record)
+                    try:
+                        player_record = get_college_player(player_college_href, player_name, player_receiving_yards)
+                        if player_record['position'] == 'WR':
+                            all_player_records.append(player_record)
+                    except Exception as e:
+                        continue
                 else:
                     continue
 
@@ -91,17 +96,23 @@ def get_college_player(college_player_href: str, player_name: str, player_receiv
         soup = BeautifulSoup(response.content, 'html.parser')
         
         #Finds the table with recieving data and finds the player position
-        table_receiving = soup.find('table', {'id': 'receiving_standard'})
-        table_receiving_body = table_receiving.find('tr')
-        cells = table_receiving.find_all('td')
-        player_position = str(cells[3].text).strip()
-        print(player_position)
-        return {
-            'player_name': player_name,
-            'position': player_position,
-            'college_receiving_yards': player_receiving_yards
-            # 'draft_pick':
-        }
+        try:
+            table_receiving = soup.find('table', {'id': 'receiving_standard'})
+            table_receiving_body = table_receiving.find('tr')
+            cells = table_receiving.find_all('td')
+            player_position = str(cells[3].text).strip()
+            print(player_position)
+            return {
+                'player_name': player_name,
+                'position': player_position,
+                'college_receiving_yards': player_receiving_yards
+                # 'draft_pick':
+            }
+        except Exception as e:
+            return {
+                'position': "NOT RECEIVER"
+            }
+        
         
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 429:
@@ -129,6 +140,16 @@ def lastName_href_list(playerName: str) -> list:
     #Use the list to find the player specific url
     # list_url = https://www.pro-football-reference.com/players + last_Name_first_Initial
 
+# def create_Nfl_Receiver_List(years: range) -> dict:
+#     preURL = 'https://www.pro-football-reference.com/years/'
+#     for year in years:
+#         proURL = preURL + str(year) + '/draft.htm'
+#         response = requests.get(proURL)
+#         response.raise_for_status()
+#         soup = BeautifulSoup(response.content, 'html.parser')
+
+#         #This rushing standard is def not the actual id name
+#         table = soup.find('table', {'id': ' rushing-standard' })
 
 
 def main():
